29d9ac54f23d3c5d08c8adc69428d1f57143efe3
Miguel S Freitas <miguel.soltofreitas.dev@outlook.com>
2026-07-16 19:15:13 -0300

    vcd

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <main>
+       <h1>MSCODE • Aula 02</h1>
+        <?php
+        const NOME EMPRESA = 'PIZARIA';
+        SvalorPizza =90.00;
+        SvalorRefrir = 15.00;
+        stotalAmigos = 4;
+        StotalFatias = 10;
+        //Calculo gorjeta e divisão da conta
+        $contaTotal = ($valorPizza + $valorRefrir) * 1.10;
+        SvalorPorAmigo = $contaTotal / $totalAmigos;
+        SatiasPorAmigo = stotalFatias / StotalAmigos;
+        SatiasQueSobraram = Stotalfatias % StotalAmigos;
+
+        echo "<h2>" . NOME_EMPRESA. "</h2>";
+
+        echo 0 valor total da conta é: R$ $contaTotal <br›";
+
+        echo "0 valor que cada amigo deve pagar é: R$ $valorPorAmigo
+        ‹br>";
+
+        ‹br>";
+        echo "Cada amigo vai comer sfatiasPorAmigo fatias de pizza
+        echo <brs":
+    </main>
+</body>
+</html>
